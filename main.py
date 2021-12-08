fc93ae4cbd2dd47b0296e1f64e037e1559a87e2a
Jose Cheble <josecheble@gmail.com>
2021-12-08 20:18:45 -0300

    Change DATABASE_URL TO DATABASE_URL1 and the posgres://' for  postgresql://

--- main.py
+++ main.py
@@ -15,13 +15,14 @@ import os
 
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = os.environ.get("SECRET_KEY", '8BYkEfBA6O6donzWlSihBXox7C0sKR6b')
+app.config['SECRET_KEY'] = os.environ.get("SECRET_KEY")
+
 ckeditor = CKEditor(app)
 Bootstrap(app)
 
 # CONNECT TO DB
 
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL",  "sqlite:///blog.db")  # One or the other
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL1",  "sqlite:///blog.db")  # One or the other
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
